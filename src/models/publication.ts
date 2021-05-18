0c4ffaceba3dbdf06de9eb128bb8bf5b14d2715d
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-05-18 21:28:34 +0300

    models

---
+++ src/models/publication.ts
@@ -0,0 +1,18 @@
+import { IPublicationItem } from "./publicationItem";
+
+export interface IPublication {
+    originalTitle: string;
+    localTitle: string;
+    isVerified: boolean;
+    countryCode: string;
+    barcode: string;
+    caseTypeName: string;
+    conditionClassName: string;
+    hasSlipCover: boolean;
+    gasHologram: boolean;
+    isRental: boolean;
+    notes: string;
+    hasTwoSidedCover: boolean;
+    hasBooklet: boolean;
+    publicationItems: IPublicationItem[];
+}
---
+++ src/models/publicationItem.ts
@@ -0,0 +1,4 @@
+export interface IPublicationItem {
+    productionOriginalTitle: string;
+    title: string;
+}
