dd288f93363dd585bbdfb7eb15d315b7a4569857
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-06-01 22:45:57 +0300

    takeLatest

--- src/redux/sagas/publicationsSagas.ts
+++ src/redux/sagas/publicationsSagas.ts
@@ -1,18 +1,14 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { fetchPublications } from '../../services/publicationsService';
 import { addPublicationsAction, getPublicationsAction } from '../actions/actions';
 
 function* getPublications(action: any) {
-    try {
-        const publications = yield call(fetchPublications, action.payload);
-        yield put(addPublicationsAction(publications));
-    } catch (e) {
-
-    }
+    const publications = yield call(fetchPublications, action.payload);
+    yield put(addPublicationsAction(publications));
 }
 
 function* publicationsSaga() {
-    yield takeEvery(getPublicationsAction().type, getPublications);
+    yield takeLatest(getPublicationsAction().type, getPublications);
 }
 
 export default publicationsSaga;
