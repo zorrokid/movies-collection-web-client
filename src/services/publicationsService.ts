d664bbb24fd1b95f58fbc2fbd79783ca75eee115
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-06-15 20:16:00 +0300

    use baseurl from config

--- src/services/publicationsService.ts
+++ src/services/publicationsService.ts
@@ -1,3 +1,5 @@
+import config from '../config';
+
 export const fetchPublications : any = async (searchString: string, token: string) => {
     const headers = {
         Authorization: `Bearer ${token}`
@@ -5,7 +7,7 @@ export const fetchPublications : any = async (searchString: string, token: strin
     const init: RequestInit = {
         headers
     };
-    let url = new URL('https://localhost:5001/Publications');
+    let url = new URL(`${config.baseUrl}//Publications`);
     url.searchParams.set('search', searchString);
     const response = await fetch(url.toString(), init);
     return response.json();
