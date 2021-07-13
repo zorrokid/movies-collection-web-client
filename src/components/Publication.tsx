6dc5d81949c09621167c9c97d99efc95f7905cae
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-07-13 14:53:09 +0300

    fix infotext for 2-sided cover

--- src/components/Publication.tsx
+++ src/components/Publication.tsx
@@ -33,7 +33,7 @@ export const PublicationCard = (props: IPublicationProps) => {
                 <Icon disabled={!item.isVerified} infoText="verified" icon=""></Icon>
                 <Icon disabled={!item.isRental} infoText="rental" icon=""></Icon>
                 <Icon disabled={!item.hasSlipCover} infoText="slip cover" icon=""></Icon>
-                <Icon disabled={!item.hasTwoSidedCover} infoText="slip cover" icon=""></Icon>
+                <Icon disabled={!item.hasTwoSidedCover} infoText="2-sided cover" icon=""></Icon>
                 <Icon disabled={!item.hasHologram} infoText="hologram" icon=""></Icon>
                 <Icon disabled={!item.hasBooklet} infoText="booklet" icon=""></Icon>
                 <FlagIcon countryCode={item.countryCode} />
