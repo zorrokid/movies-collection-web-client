5844f4bf6c5c4f60dedc229af6d78f65c33463a3
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-07-29 13:53:33 +0300

    refactor

--- src/components/Publication.tsx
+++ src/components/Publication.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import Box from '@material-ui/core/Box';
 import { Publication } from '../models/publication';
 import { PublicationItems } from './PublicationItems';
 import { Icon } from './Icon';
@@ -8,36 +8,30 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import { ConditionIcon } from './icon-components/ConditionIcon';
 
-const ItemContent = styled.div`
-`;
-
-const IconsContainer = styled.div`
-`;
-
 interface IPublicationProps {
-    item: Publication;
+    publication: Publication;
 }
 
 export const PublicationCard = (props: IPublicationProps) => {
-    const {item} = props;
+    const { publication } = props;
     return (
         <Card>
-            <CardHeader title={item.originalTitle} />
+            <CardHeader title={publication.originalTitle} />
             <CardContent>
-                <ItemContent>{item.localTitle}</ItemContent>
-                <ItemContent>{item.barcode}</ItemContent>
-                <ItemContent>Condition: {item.conditionClassName}</ItemContent>
-                <IconsContainer>
-                    <ConditionIcon conditionClass={item.ConditionClassId} />
-                    <Icon disabled={!item.isVerified} infoText="verified" icon=""></Icon>
-                    <Icon disabled={!item.isRental} infoText="rental" icon=""></Icon>
-                    <Icon disabled={!item.hasSlipCover} infoText="slip cover" icon=""></Icon>
-                    <Icon disabled={!item.hasTwoSidedCover} infoText="2-sided cover" icon=""></Icon>
-                    <Icon disabled={!item.hasHologram} infoText="hologram" icon=""></Icon>
-                    <Icon disabled={!item.hasBooklet} infoText="booklet" icon=""></Icon>
-                    <FlagIcon countryCode={item.countryCode} />
-                </IconsContainer>
-                <PublicationItems items={item.publicationItems} />
+                <Box>{publication.localTitle}</Box>
+                <Box>{publication.barcode}</Box>
+                <Box>Condition: {publication.conditionClassName}</Box>
+                <Box>
+                    <ConditionIcon conditionClassId={publication.conditionClassId} />
+                    <Icon disabled={!publication.isVerified} infoText="verified" icon=""></Icon>
+                    <Icon disabled={!publication.isRental} infoText="rental" icon=""></Icon>
+                    <Icon disabled={!publication.hasSlipCover} infoText="slip cover" icon=""></Icon>
+                    <Icon disabled={!publication.hasTwoSidedCover} infoText="2-sided cover" icon=""></Icon>
+                    <Icon disabled={!publication.hasHologram} infoText="hologram" icon=""></Icon>
+                    <Icon disabled={!publication.hasBooklet} infoText="booklet" icon=""></Icon>
+                    <FlagIcon countryCode={publication.countryCode} />
+                </Box>
+                <PublicationItems items={publication.publicationItems} />
             </CardContent>
         </Card>
     )
